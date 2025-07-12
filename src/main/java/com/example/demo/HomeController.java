13b9c9883e9f1b3306da2803227069aafe0e6e6d
kazushi-dev <kazushi8160@gmail.com>
2025-07-13 03:59:20 +0900

    Set / to redirect to /reception

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Heroku deployment successful!";
+        return "redirect:/reception"; // トップアクセス時に /reception へ転送
     }
 }
